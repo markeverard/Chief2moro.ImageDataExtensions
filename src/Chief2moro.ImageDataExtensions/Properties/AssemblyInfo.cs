3421f04f9d87e539a1aa3220e4b117424d6712c5
Mark Everard <markeverard@hotmail.com>
2018-01-31 10:02:35 +0000

    Update Nuget Upgrade to CMS 11

--- src/Chief2moro.ImageDataExtensions/Properties/AssemblyInfo.cs
+++ src/Chief2moro.ImageDataExtensions/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Chief2moro.ImageDataExtensions")]
-[assembly: AssemblyDescription("A library allowing EPiServer developers to add image size validation and resizing for the EPiServer 10 CMS ImageData content type")]
+[assembly: AssemblyDescription("A library allowing EPiServer developers to add image size validation and resizing for the EPiServer CMS ImageData content type")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Mark Everard")]
 [assembly: AssemblyProduct("Chief2moro.ImageDataExtensions")]
-[assembly: AssemblyCopyright("Copyright © Chief2moro 2017")]
+[assembly: AssemblyCopyright("Copyright © Chief2moro 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("11.0.0.0")]
+[assembly: AssemblyFileVersion("11.0.0.0")]
